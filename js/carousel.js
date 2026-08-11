fe12c4d48e9a5718e0be619406b8d0fa1381a3d8
antoniosmatos-prog <antonio.s.matos@ba.estudante.senai.br>
2026-08-11 11:47:37 -0300

    criada pag home

--- js/carousel.js
+++ js/carousel.js
@@ -8,6 +8,11 @@ let carouselArr = [];
 
 //class Carousel
 class Carousel {
+    constructor(imagem, titulo, link){
+        this.imagem = imagem;
+        this.titulo = titulo;
+        this.link = link;
+    }
 
     
       
@@ -27,6 +32,48 @@ class Carousel {
     }
 
     static Next(){
-        
+        let caroucel = Carousel._sequence;
+
+        let imagem = document.getElementById("carousel");
+
+        imagem.innerHTML = `<img src="img/${carouselArr[caroucel].imagem}" alt="${carouselArr[caroucel].titulo}" />`
+
+        let titulo = document.getElementById("carousel-title");
+        titulo.innerHTML = `<a href="${carouselArr[caroucel].link}"><h2>${carouselArr[caroucel].titulo}</h2></a>`
+
+        Carousel._sequence++;
+
+        if(Carousel._sequence >= Carousel._size){
+            Carousel._sequence = 0;
+        }
     }
+
+    static photo1(){
+        Carousel._sequence = 0;
+        Carousel.Next();
+    }
+
+    static photo2(){
+        Carousel._sequence = 1;
+        Carousel.Next();
+    }
+
+    static photo3(){
+        Carousel._sequence = 2;
+        Carousel.Next();
+    }
+
+        
 };
+
+function photo1(){
+    Carousel.photo1();
+}
+
+function photo2(){
+    Carousel.photo2();
+}
+
+function photo3(){
+    Carousel.photo3();
+}
