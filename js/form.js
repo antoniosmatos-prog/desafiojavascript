4fe92867cbe0729800acd73db5c09d66fbc59e57
antoniosmatos-prog <antonio.s.matos@ba.estudante.senai.br>
2026-08-17 11:34:49 -0300

    alterado termos e condições

--- js/form.js
+++ js/form.js
@@ -15,22 +15,33 @@ class contato {
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-            console.log(data);
-  
+    let marcarbox = document.querySelector("#checara")
+
+    if (marcarbox.checked){
+
+        let data = new contato(form.elements.namedItem("nome").value,
+                form.elements.namedItem("sobrenome").value, 
+                form.elements.namedItem("email").value, 
+                form.elements.namedItem("cpf").value, 
+                form.elements.namedItem("telefone").value, 
+                form.elements.namedItem("contato").value);
+                console.log(data);
+    }
 }
 
 function Enviar() {
 
-    var nome = document.getElementById("nomeid");
+    let marcarbox = document.querySelector("#checara")
 
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    if (marcarbox.checked){
+            var nome = document.getElementById("nomeid");
+
+        if (nome.value != "") {
+            alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+        }
     }
 
+    else{
+        alert("Seus dados só serão enviados após você marcar: Sim. Lí e concordo com os Termos e Condições.")
+    }
 }
